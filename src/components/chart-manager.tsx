03a8f6ba05096f4dcebba40726ec9e87b9beb543
Rishabh <irishabhyadav90@gmail.com>
2024-11-22 15:15:46 +0400

    feat: Add folder and component structure

---
+++ src/components/chart-manager.tsx
@@ -0,0 +1,13 @@
+import ChartForm from "./chart-form"
+import ChartList from "./chart-list"
+
+const ChartManager = () => {
+  return (
+    <div>
+       <ChartForm />
+       <ChartList /> 
+    </div>
+  )
+}
+
+export default ChartManager
