272541c377ed757618b30d9e40f482b8a0930a47
Rishabh <irishabhyadav90@gmail.com>
2024-11-23 17:45:28 +0400

    feat: Add form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import './App.css'
-import ChartManager from './components/chart-manager'
+import './App.css';
+import ChartManager from './components/chart-manager';
 
 function App() {
-
   return (
-    <div className='min-h-screen bg-gray-50 w-full'>
-      <main className='p-4'>
+    <div className="min-h-screen bg-gray-50 w-full">
+      <main className="p-4">
         <ChartManager />
       </main>
     </div>
-  )
-
+  );
 }
 
-export default App
+export default App;
